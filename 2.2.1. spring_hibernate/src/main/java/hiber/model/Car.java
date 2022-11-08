8ab44d380dac5d0a7b874992e63a9239918896bc
alexe <prograbota@mail.ru>
2022-11-08 16:02:47 +0300

    class Car annotation

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -16,7 +16,12 @@ public class Car {
     @Column(name = "series")
     private int series;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "users_id")
+    private User user;
+
     public Car() {}
+
 
     public Car(String model, int series) {
         this.model = model;
@@ -31,4 +36,22 @@ public class Car {
 
     public int getSeries() {return series;}
     public void setSeries(int series) {this.series = series;}
+
+    public User getUser(User user) {
+        return user;
+    }
+
+    public User setUser(User user) {
+        this.user = user;
+        return user;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
 }
